95046e15d233a86a80328fab9ef91736416211e8
Timothy Sharkey <Techne3@gmail.com>
2020-12-22 11:20:52 -0700

    Added depens. for speechly material-ui - uuid and changed the react version back to 16.12.0 - Created Card Components using material-ui

---
+++ src/components/Details/DetailsCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Card, CardHeader, CardContent, Typography } from "@material-ui/core";
+import { Doughnut } from "react-chartjs-2";
+
+import useStyles from "./styles";
+
+const DetailsCard = () => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.income}>
+      <CardHeader />
+      <CardContent>
+        <Typography variant="h5">$</Typography>
+        {/* <Doughnut /> */}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default DetailsCard;
