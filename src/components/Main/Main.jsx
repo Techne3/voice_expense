0521318bea39bf86b08fdb75f482a4f8df519233
Timothy Sharkey <Techne3@gmail.com>
2020-12-27 11:35:19 -0700

    List of transactions component layout - style for List component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,31 +1,44 @@
-import React from 'react'
-import {Card, CardHeader, CardContent, Typography, Grid, Divider} from '@material-ui/core'
-import useStyles  from './styles'
-import Form from './Form/Form.jsx'
-const  Main = () => {
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+} from "@material-ui/core";
 
-    const classes = useStyles()
+import useStyles from "./styles";
 
-    return (
-        <Card className={classes.root}>
-            <CardHeader title="Expense Tracker" subheader ="Powered by Speechly "/> 
-            <CardContent>
-                <Typography align="center" variant="h5">Total Balance $119</Typography>
-                <Typography variant="subtitle1" style={{lineHeight:'1.5rem', marginTop:'200px'}}>
-                   {/* cardInfo */}
-                    Try saying: Add income for $100 in Category Salary for Monday... 
-                </Typography>
-                <Divider />
-                <Form />
-            </CardContent>
-            <CardContent className={classes.CardContent}>
-                <Grid item xs={12}>
-                    {/* <List /> */}
-                </Grid>
-            </CardContent>
-        </Card>
-    )
-}
+import Form from "./Form/Form.jsx";
+import List from "./List/List";
+const Main = () => {
+  const classes = useStyles();
 
-export default Main
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" subheader="Powered by Speechly " />
+      <CardContent>
+        <Typography align="center" variant="h5">
+          Total Balance $119
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{ lineHeight: "1.5rem", marginTop: "200px" }}
+        >
+          {/* cardInfo */}
+          Try saying: Add income for $100 in Category Salary for Monday...
+        </Typography>
+        <Divider />
+        <Form />
+      </CardContent>
+      <CardContent className={classes.CardContent}>
+        <Grid item xs={12}>
+          <List />
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
 
+export default Main;
