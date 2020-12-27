e28263d44c6be20575f1fc71234617227d489bcf
Timothy Sharkey <Techne3@gmail.com>
2020-12-27 10:59:34 -0700

    Form component layout outlined - styling for form on the main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Card, CardHeader, CardContent, Typography, Grid, Divider} from '@material-ui/core'
 import useStyles  from './styles'
+import Form from './Form/Form.jsx'
 const  Main = () => {
 
     const classes = useStyles()
@@ -11,10 +12,11 @@ const  Main = () => {
             <CardContent>
                 <Typography align="center" variant="h5">Total Balance $119</Typography>
                 <Typography variant="subtitle1" style={{lineHeight:'1.5rem', marginTop:'200px'}}>
-                    {/* {} */}
+                   {/* cardInfo */}
                     Try saying: Add income for $100 in Category Salary for Monday... 
                 </Typography>
                 <Divider />
+                <Form />
             </CardContent>
             <CardContent className={classes.CardContent}>
                 <Grid item xs={12}>
