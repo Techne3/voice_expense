d42c5786ef21fb4f6b41944f8dcf0ac8838a059c
Timothy Sharkey <Techne3@gmail.com>
2021-01-10 16:31:11 -0700

    speechly config

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { SpeechProvider } from "@speechly/react-client";
 
 import App from "./App";
 import "./index.css";
@@ -7,9 +8,11 @@ import "./index.css";
 import { Provider } from "./context/context";
 
 ReactDOM.render(
-  <Provider>
-    <App />
-  </Provider>,
+  <SpeechProvider appId="20c2c8a3-6903-4000-a478-a0729572cf7a" language="en-US">
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>,
 
   document.getElementById("root")
 );
